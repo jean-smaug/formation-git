879edafa30139174ee1507c3a86ccc2f02ba64d3
you@example.com <you@example.com>
2017-04-06 14:13:29 +0200

    add a fecth to jsonplaceholder

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var fetch = require('node-fetch');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/demo', function (req, res) {
+  console.log('demo');
+  var url = 'https://jsonplaceholder.typicode.com/posts/1';
+  fetch(url)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        console.log(json);
+        res.send(json);
+    });
 });
 
 app.listen(3000, function () {
